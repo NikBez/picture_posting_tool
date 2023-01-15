05cf52c131ef6a27719a4bd421b24602c39dfdb0
Kit <nikitabit@yandex.ru>
2023-01-15 17:29:44 +0700

    save image from Space X API

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import requests
 from pathlib import Path
 
+default_images_path = "images/"
+
 def main():
-    picture_url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
+    API_spaceX_patch = 'https://api.spacexdata.com/v5/launches/5eb87d47ffd86e000604b38a'
+    pictures = fetch_spacex_last_launch(API_spaceX_patch)
+    save_image_by_link(pictures, default_images_path)
 
 
-def save_image_by_link(link, path):
+def save_image_by_link(link, save_path):
 
-    pict = requests.get(link)
-    pict.raise_for_status()
+    Path(save_path).mkdir(parents=True, exist_ok=True)
 
-    Path(path).mkdir(parents=True, exist_ok=True)
+    for index, pic_link in enumerate(fligth_picks, start=1):
+        response = requests.get(pic_link)
+        response.raise_for_status()
+        picture = response.content
+        with open(f"{save_path}/{index}.jpg", "wb") as pic_file:
+            pic_file.write(picture)
 
-    with open("images/habl.jpg", "wb") as picture:
-        picture.write(pict.content)
+def fetch_spacex_last_launch(API_patch):
 
+    response = requests.get(API_patch)
+    response.raise_for_status()
+    return response.json()["links"]["flickr"]["original"]
 
 if __name__ == "__main__":
     main()
