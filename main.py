acd2669d355c456d432604d05083ad337f2d4269
Kit <nikitabit@yandex.ru>
2023-01-16 10:24:05 +0700

    download photos from NASA.

--- main.py
+++ main.py
@@ -1,30 +1,90 @@
+from datetime import datetime, timedelta
+import os
 import requests
 from pathlib import Path
+from dotenv import load_dotenv
+from urllib import parse
 
 default_images_path = "images/"
 
 def main():
-    API_spaceX_patch = 'https://api.spacexdata.com/v5/launches/5eb87d47ffd86e000604b38a'
-    pictures = fetch_spacex_last_launch(API_spaceX_patch)
+
+    load_dotenv()
+
+    nasa_api_token = os.environ['NASA_API_TOKEN']
+    spacex_api_url = os.environ["SPACEX_API_URL"]
+    nasa_api_url = os.environ["NASA_API_URL"]
+    nasa_epic_url = os.environ["NASA_EPIC_DATA_URL"]
+    nasa_epic_picture_url = os.environ["NASA_EPIC_PICTURE_URL"]
+
+    pictures = []
+    pictures += fetch_spacex_launches(spacex_api_url)
+    pictures += fetch_nasa_pics(nasa_api_url, nasa_api_token)
+    pictures += fetch_epic_pics(nasa_epic_url, nasa_epic_picture_url, nasa_api_token)
+
     save_image_by_link(pictures, default_images_path)
 
 
-def save_image_by_link(link, save_path):
+def save_image_by_link(links, save_path):
 
     Path(save_path).mkdir(parents=True, exist_ok=True)
 
-    for index, pic_link in enumerate(fligth_picks, start=1):
-        response = requests.get(pic_link)
+    for index, link in enumerate(links, start=1):
+        response = requests.get(link)
         response.raise_for_status()
         picture = response.content
-        with open(f"{save_path}/{index}.jpg", "wb") as pic_file:
+        extention = get_image_extention(link)
+        with open(f"{save_path}/space-{index}{extention}", "wb") as pic_file:
             pic_file.write(picture)
 
-def fetch_spacex_last_launch(API_patch):
+def fetch_spacex_launches(api_url):
 
-    response = requests.get(API_patch)
+    pictures = []
+    response = requests.get(api_url)
     response.raise_for_status()
-    return response.json()["links"]["flickr"]["original"]
+
+    flights = response.json()
+    for flight in flights[:20]:
+        flight_picks = flight["links"]["flickr"]["original"]
+        pictures += flight_picks
+
+    return pictures
+
+def fetch_nasa_pics(api_url, nasa_api_token):
+
+    pictures = []
+
+    start_date = datetime.now() - timedelta(days=10)
+    api_params = {'api_key': nasa_api_token,
+                  'start_date': start_date.strftime("%Y-%m-%d")
+                  }
+
+    response = requests.get(api_url, params=api_params)
+    response.raise_for_status()
+    pictures_with_data = response.json()
+    for picture in pictures_with_data:
+        pictures.append(picture['url'])
+
+    return pictures
+
+
+def fetch_epic_pics(epic_data_url, epic_picture_url, nasa_api_token):
+
+    pictures = []
+    response = requests.get(epic_data_url, params={'api_key': nasa_api_token, })
+    response.raise_for_status()
+    for image_data in response.json():
+        picture_name = image_data['image']
+        picture_date = datetime.strptime(image_data['date'], "%Y-%m-%d %H:%M:%S")
+        pictures.append(
+            f"{epic_picture_url}{picture_date.year}/{picture_date.month:02d}/{picture_date.day:02d}/png/{picture_name}.png?api_token={nasa_api_token}")
+    return pictures
+
+
+def get_image_extention(image_link):
+
+    path_from_url = parse.urlparse(image_link).path
+    return os.path.splitext(path_from_url)[1]
 
 if __name__ == "__main__":
     main()
